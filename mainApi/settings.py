25d7ff73a44f2b4572174445a8233d9dc1bbd1c0
Luis Guareschi <67588689+luisguareschi@users.noreply.github.com>
2025-06-27 12:48:23 +0200

    Add docker compose config (#1)
    * Enhance settings for environment variable support, update .gitignore to include media and static files, and adjust database configuration for development mode.
    
    * Update GitHub Actions workflow to trigger on 'master' branch instead of 'main'
    
    * Update GitHub Actions workflow to use Python 3.12 and change health check URL to /admin
    
    * Rename GitHub Actions workflow from 'Test Django Migrations and Run' to 'Main Workflow'
    
    * Rename service from 'streamer-backend' to 'baseapp-backend' in docker-compose.yml and update requirements.txt

--- mainApi/settings.py
+++ mainApi/settings.py
@@ -11,6 +11,14 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 from datetime import timedelta
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+from django.core.management.utils import get_random_secret_key
+import sys
+import dj_database_url
+
+# Load environment variables
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +28,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$+4vof5i#a#uwg4itox5w_57-=k%317(8(0yio!)t8gt9=if5r'
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG", "False") == "True"
+DEVELOPMENT_MODE = os.getenv("DEVELOPMENT_MODE", "False") == "True"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -83,12 +92,19 @@ WSGI_APPLICATION = 'mainApi.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if DEVELOPMENT_MODE is True or os.getenv("DATABASE_URL", None) is None:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        }
     }
-}
+elif len(sys.argv) > 0 and sys.argv[1] != 'collectstatic':
+    if os.getenv("DATABASE_URL", None) is None:
+        raise Exception("DATABASE_URL environment variable not defined")
+    DATABASES = {
+        "default": dj_database_url.parse(os.environ.get("DATABASE_URL")),
+    }
 
 
 # Password validation
@@ -126,6 +142,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
@@ -169,5 +186,11 @@ SPECTACULAR_SETTINGS = {
  'SERVE_INCLUDE_SCHEMA': False,
 }
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-
+# UPDATE IF NEEDED TO ALLOW DEPLOYMENT IN THIS DOMAINS
+CSRF_TRUSTED_ORIGINS = [
+    'https://*.ondigitalocean.app',
+    'https://*.digitaloceanspaces.com',
+]
